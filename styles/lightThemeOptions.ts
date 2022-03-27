08fe7be4eb160a6fcfc34c6c05a9ea1598c6ea33
Angael <krzysztofwidacki@gmail.com>
2022-03-27 12:13:48 +0200

    init2

--- styles/lightThemeOptions.ts
+++ styles/lightThemeOptions.ts
@@ -4,6 +4,13 @@ const lightThemeOptions: ThemeOptions = {
     palette: {
         mode: 'light',
     },
+    components: {
+        MuiButton: {
+            defaultProps: {
+                disableRipple: true,
+            },
+        },
+    },
 };
 
 export default lightThemeOptions;
