dcaaf7914ce873df5e9057bb350d42b3fe55bfb2
Angael <krzysztofwidacki@gmail.com>
2022-03-27 12:11:46 +0200

    init

---
+++ components/home/styled.ts
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material';
+
+export const TitleContainer = styled('header')({
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: '100vh',
+    flexDirection: 'column',
+    alignItems: 'center',
+});
